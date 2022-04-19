1b44ed0c8f0c274885e1e7d6eb6e5700643af9b2
JosueRodriguez38 <josue.rodriguez38@upr.edu>
2022-04-19 19:44:20 -0400

    added traffic type to all values in map

--- mainCode.py
+++ mainCode.py
@@ -1,16 +1,17 @@
 import search
+trafficType = ['heavy','mixed','light'] 
 
-PR_map = search.Map({('Ponce','Salinas'):  36,('Ponce','Adjuntas'):  25,('Ponce','Yauco'):  32,
-('Mayaguez','Yauco'):  45,('Mayaguez','Maricao'):  26,('Mayaguez','Rincon'):  23,('Mayaguez','Aguadilla'):  28,
-('Rincon','Aguadilla'):  17,('Camuy','Aguadilla'):  39,('Camuy','Utuado'):  44,('Camuy','Arecibo'):  15,
-('Utuado','Arecibo'):  33,('Utuado','Adjuntas'):  21,('Adjuntas','Maricao'):  54,
-('Bayamon','Arecibo'):  77,('Bayamon','Catano'):  11,('Bayamon','SanJuan'):  19,
-('SanJuan','Cayey'):  55,('SanJuan','Caguas'):  33,('SanJuan','Canovanas'):  30,
-('Cayey','Salinas'):  32,('Cayey','Guayama'):  42,('Cayey','Caguas'):  24,
-('Caguas','Juncos'):  15,
-('Canovanas','Juncos'):  21,('Canovanas','Fajardo'):  31,
-('Humacao','Fajardo'):  36,('Humacao','Yabucoa'):  18,
-('Guayama','Salinas'):  23,('Guayama','Yabucoa'):  38,})
+PR_map = search.Map({('Ponce','Salinas'):  [36,trafficType[0]],('Ponce','Adjuntas'):  [25,trafficType[2]],('Ponce','Yauco'):  32,
+('Mayaguez','Yauco'):  [45,trafficType[0]],('Mayaguez','Maricao'):  [26,trafficType[1]],('Mayaguez','Rincon'):  [23,trafficType[0]],('Mayaguez','Aguadilla'):  [28,trafficType[0]],
+('Rincon','Aguadilla'):  [17,trafficType[0]],('Camuy','Aguadilla'):  [39,trafficType[0]],('Camuy','Utuado'):  [44,trafficType[2]],('Camuy','Arecibo'):  [15,trafficType[0]],
+('Utuado','Arecibo'):  [33,trafficType[1]],('Utuado','Adjuntas'):  [21,trafficType[2]],('Adjuntas','Maricao'):  [54,trafficType[2]],
+('Bayamon','Arecibo'):  [77,trafficType[0]],('Bayamon','Catano'):  [11,trafficType[1]],('Bayamon','SanJuan'):  [19,trafficType[0]],
+('SanJuan','Cayey'):  [55,trafficType[0]],('SanJuan','Caguas'):  [33,trafficType[0]],('SanJuan','Canovanas'):  [30,trafficType[1]],
+('Cayey','Salinas'):  [32,trafficType[1]],('Cayey','Guayama'):  [42,trafficType[1]],('Cayey','Caguas'):  [24,trafficType[0]],
+('Caguas','Juncos'):  [15,trafficType[1]],
+('Canovanas','Juncos'):  [21,trafficType[1]],('Canovanas','Fajardo'):  [31,trafficType[0]],
+('Humacao','Fajardo'):  [36,trafficType[0]],('Humacao','Yabucoa'):  [18,trafficType[1]],
+('Guayama','Salinas'):  [23,trafficType[0]],('Guayama','Yabucoa'):  [38,trafficType[1]]})
 
 
 #Velocidades
