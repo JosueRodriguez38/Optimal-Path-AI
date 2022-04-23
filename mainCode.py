7a3dfabe0fc93b89bb3dfcddfe118a03c8a93146
JosueRodriguez38 <josue.rodriguez38@upr.edu>
2022-04-23 19:39:50 -0400

    organized file after pull

--- mainCode.py
+++ mainCode.py
@@ -1,25 +1,4 @@
 import search
-locs = {'SanJuan':(2044227.08, 805685.13), 'Ponce':(1993038.45,752625.54),'Salinas':(1989776.68,786168.85),
-        'Adjuntas':(2009766.0,740860.58),'Yauco':(1995376.04,727613.80), 'Maricao':(2010542.0,717323.0),
-        'Rincon':(2028715.19, 684922.70), 'Aguadilla':(2038469.91,694965.94),'Camuy':(2041508.70,726682.74),
-        'Utuado':(2021090.65,743122.28), 'Arecibo':(2040952.74, 748582.15),'Bayamon':(2035606.90, 799519.64),
-        'Catano':(2041984.68, 802564.11), 'Cayey':(2004874.15,799924.19), 'Caguas':(2019146.30,813544.32),
-        'Juncos':(2017815.84, 191065.79),'Canovanas':(2034106.34,193574.50),
-        'Humacao':(2009046.63, 200840.64), 'Yabucoa':(1998149.64, 195169.11), 'Guayama':(1990808.29, 805675.48),
-        'Fajardo':(2028271.61, 219651.35), 'Mayaguez':(2013453.29,696161.94)}
-PR_map = search.Map({('Ponce','Salinas'):  36,('Ponce','Adjuntas'):  25,('Ponce','Yauco'):  32,
-('Mayaguez','Yauco'):  45,('Mayaguez','Maricao'):  26,('Mayaguez','Rincon'):  23,('Mayaguez','Aguadilla'):  28,
-('Rincon','Aguadilla'):  17,('Camuy','Aguadilla'):  39,('Camuy','Utuado'):  44,('Camuy','Arecibo'):  15,
-('Utuado','Arecibo'):  33,('Utuado','Adjuntas'):  21,('Adjuntas','Maricao'):  54,
-('Bayamon','Arecibo'):  77,('Bayamon','Catano'):  11,('Bayamon','SanJuan'):  19,
-('SanJuan','Cayey'):  55,('SanJuan','Caguas'):  33,('SanJuan','Canovanas'):  30,
-('Cayey','Salinas'):  32,('Cayey','Guayama'):  42,('Cayey','Caguas'):  24,
-('Caguas','Juncos'):  15,
-('Canovanas','Juncos'):  21,('Canovanas','Fajardo'):  31,
-('Humacao','Fajardo'):  36,('Humacao','Yabucoa'):  18,
-('Guayama','Salinas'):  23,('Guayama','Yabucoa'):  38,},locs)
-#locs= 'City':(UTM_Northing, UTM_Easting)
-
 from datetime import datetime
 
 t = datetime.now()
@@ -37,6 +16,16 @@ def arrivalTime(dist, vel, trafficVol):
     else:
         return  opmltrvltime
 
+
+
+locs = {'SanJuan':(2044227.08, 805685.13), 'Ponce':(1993038.45,752625.54),'Salinas':(1989776.68,786168.85),
+        'Adjuntas':(2009766.0,740860.58),'Yauco':(1995376.04,727613.80), 'Maricao':(2010542.0,717323.0),
+        'Rincon':(2028715.19, 684922.70), 'Aguadilla':(2038469.91,694965.94),'Camuy':(2041508.70,726682.74),
+        'Utuado':(2021090.65,743122.28), 'Arecibo':(2040952.74, 748582.15),'Bayamon':(2035606.90, 799519.64),
+        'Catano':(2041984.68, 802564.11), 'Cayey':(2004874.15,799924.19), 'Caguas':(2019146.30,813544.32),
+        'Juncos':(2017815.84, 191065.79),'Canovanas':(2034106.34,193574.50),
+        'Humacao':(2009046.63, 200840.64), 'Yabucoa':(1998149.64, 195169.11), 'Guayama':(1990808.29, 805675.48),
+        'Fajardo':(2028271.61, 219651.35), 'Mayaguez':(2013453.29,696161.94)}
 PR_map = search.Map({('Ponce','Salinas'):  [36,50,trafficType[0]],('Ponce','Adjuntas'):  [25,40,trafficType[2]],('Ponce','Yauco'):  [32,55,trafficType[0]],
 ('Mayaguez','Yauco'):  [45,55,trafficType[0]],('Mayaguez','Maricao'):  [26,40,trafficType[1]],('Mayaguez','Rincon'):  [23,50,trafficType[0]],('Mayaguez','Aguadilla'):  [28,55,trafficType[0]],
 ('Rincon','Aguadilla'):  [17,55,trafficType[0]],('Camuy','Aguadilla'):  [39,50,trafficType[0]],('Camuy','Utuado'):  [44,40,trafficType[2]],('Camuy','Arecibo'):  [15,50,trafficType[0]],
@@ -47,8 +36,9 @@ PR_map = search.Map({('Ponce','Salinas'):  [36,50,trafficType[0]],('Ponce','Adju
 ('Caguas','Juncos'):  [15,50,trafficType[1]],
 ('Canovanas','Juncos'):  [21,50,trafficType[1]],('Canovanas','Fajardo'):  [31,50,trafficType[0]],
 ('Humacao','Fajardo'):  [36,55,trafficType[0]],('Humacao','Yabucoa'):  [18,55,trafficType[1]],
-('Guayama','Salinas'):  [23,50,trafficType[0]],('Guayama','Yabucoa'):  [38,50,trafficType[1]]})
+('Guayama','Salinas'):  [23,50,trafficType[0]],('Guayama','Yabucoa'):  [38,50,trafficType[1]]},locs)
 
+#locs= 'City':(UTM_Northing, UTM_Easting)
 
 
 g = search.GraphProblem('Mayaguez', "SanJuan", PR_map)
