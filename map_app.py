240a834725d3308ecbda6792f6d909c49d1700f7
JosueRodriguez38 <josue.rodriguez38@upr.edu>
2022-05-06 13:00:37 -0400

    changed velocity values

--- map_app.py
+++ map_app.py
@@ -10,7 +10,8 @@ def run_app():
     if goal not in mainCode.locs:
         print("City not in database, please try again.")
         return run_app()
-    ucs = search.uniform_cost_search(search.RouteProblem(initial, goal, map=mainCode.PR_map))
-    print("The estimated time is ", ucs.path_cost,"minutes. The optimal path is:",search.path_states(ucs),'.' )
+    
+    BUCS = search.bidirectional_uniform_cost_search(search.RouteProblem(initial, goal, map=mainCode.PR_map))
+    print("The estimated time is ", BUCS.path_cost,"minutes. The optimal path is:",search.path_states(BUCS),'.' )
     return
 print(run_app())
