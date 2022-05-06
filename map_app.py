2ec053f0080296df232d4de8fbc6c92efb2ae624
JosueRodriguez38 <josue.rodriguez38@upr.edu>
2022-05-06 17:31:47 -0400

    displays all city options on run

--- map_app.py
+++ map_app.py
@@ -1,7 +1,9 @@
 import mainCode
 import search
 
+cities = "SanJuan  -  Ponce  -  Salinas  -  Adjuntas   -  Yauco  -  Maricao   -  Rincon   -  Auadilla   -  Camuy   -  Utuado   -  Arecibo   -  Bayamon   -  Catano   -  Cayey   -  Caguas   -  Juncos   -  Canovanas   -  Humacao   -  Yabucoa   -  Guayama   -  Fajardo,   -  Mayaguez"  
 def run_app():
+    print("Options to travel from and towards to: ",  cities)
     initial = input("Enter your current town: ")
     if initial not in mainCode.locs:
         print("City not in database, please try again.")
